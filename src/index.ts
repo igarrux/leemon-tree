b02a93f8f8d880070d22b10882b930d1c5fa2f92
garruxx <garruxx@gmail.com>
2025-07-25 12:08:45 -0300

    feat: :sparkles: adds auto-yes option
    Adds the ability to automatically answer prompts in the CLI using the `LT_AUTO_YES` environment variable.
    
    This allows for non-interactive execution of the CLI, which is useful in automated environments.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import prompts from 'prompts';
 import { cli } from './cli/cli.js';
 
 export function runIfDirect(metaUrl = import.meta.url, argv = process.argv) {
+    const autoYes = process.env.LT_AUTO_YES === 'true';
     if (metaUrl === `file://${argv[1]}`) {
-        if (argv.includes('--yes')) prompts.override({ delete: true });
+        if (argv.includes('--yes') || autoYes) prompts.override({ delete: true });
+        if (autoYes) prompts.override({ action: 'yes-to-all' });
         cli();
     }
 }
